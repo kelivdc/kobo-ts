bebac09b02fcc28e634750ae3537d2ac7a4d176f
Arie <kelivdc@yahoo.com>
2023-08-14 22:16:39 +0700

    Assets Local DB

--- app/api/auth/[...nextauth]/route.jsx
+++ app/api/auth/[...nextauth]/route.jsx
@@ -35,7 +35,7 @@ export const authOptions = {
     secret: process.env.NEXTAUTH_SECRET,
     session: {
         strategy: "jwt",
-    },
+    },    
     pages: {
         signIn: "/",      
         error: '/auth/error'
--- app/cms/assets/page.jsx
+++ app/cms/assets/page.jsx
@@ -14,19 +14,16 @@ import { useSession } from 'next-auth/react'
 import { useState, useEffect } from "react"
 import moment from 'moment'
 import { Button } from "@/components/ui/button"
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover"
-import { ReloadIcon } from "@radix-ui/react-icons";
-
+import { Skeleton } from "@/components/ui/skeleton"
+import Link from 'next/link'
 
 export default function Assets() {
   const { data: session } = useSession()
   const [assets, setAssets] = useState([])
   const [isOpen, SetIsOpen] = useState(true)
   const [uid, setUid] = useState(null)
+  const [isLoading, setLoading] = useState(true);
+
   const selectUid = (uid) => {
     setUid(uid);
     document.getElementById(`btn_${uid}`).innerHTML = 'Pulling...';
@@ -35,8 +32,24 @@ export default function Assets() {
     pullData(uid)
   }
   const api_url = process.env.server
+
   useEffect(() => {
+    var arrResult = []
     if (session) {
+      async function getTotal(uid) {
+        try {
+          const resp = await fetch(api_url + '/assets/total/' + uid, {
+            headers: {
+              Authorization: `Bearer ${session.jwt}`
+            }
+          })
+          const data = await resp.json()
+          return data['Count']
+        } catch {
+          return 0
+        }
+      }
+
       async function getAssets() {
         const resp = await fetch(api_url + '/assets', {
           headers: {
@@ -44,11 +57,19 @@ export default function Assets() {
           }
         })
         const data = await resp.json()
-        setAssets(data['results'])
+        let hasil = data['results']
+        var arr = {}
+        hasil.forEach(item => {
+          arr['result'] = item
+          arr['local'] = getTotal(item['uid'])
+          arrResult.push(arr)
+        })
+        setAssets(arrResult)
       }
+      setLoading(false);
       getAssets()
     }
-  }, [])
+  }, [session])
   const pullData = async (uid) => {
     const resp = await fetch(process.env.server + `/assets/pull/${uid}`, {
       headers: {
@@ -64,32 +85,36 @@ export default function Assets() {
   return (
     <>
       <Panel title="Assets">
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>Name</TableHead>
-              <TableHead>Create At</TableHead>
-              <TableHead>Last Modified</TableHead>
-              <TableHead>Submissions</TableHead>
-              <TableHead>Local</TableHead>
-              <TableHead>Actions</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {assets.map((item, index) => (
-              <TableRow key={index}>
-                <TableCell>{item.name}</TableCell>
-                <TableCell>{moment(item.date_created).format('LL')}</TableCell>
-                <TableCell>{moment(item.date_modified).format('LL')}</TableCell>
-                <TableCell>{item.deployment__submission_count}</TableCell>
-                <TableCell id={`total_${item.uid}`}>0</TableCell>
-                <TableCell>
-                  <Button variant="destructive" id={`btn_${item.uid}`} className="rounded-full" onClick={() => selectUid(item.uid)}>Pull</Button>
-                </TableCell>
+        {isLoading ? (
+          <Skeleton className="w-[100px] h-[20px] rounded-full" />
+        ) : (
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Name</TableHead>
+                <TableHead>Create At</TableHead>
+                <TableHead>Last Modified</TableHead>
+                <TableHead>Submissions</TableHead>
+                <TableHead>Local Subs.</TableHead>
+                <TableHead>Actions</TableHead>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+            </TableHeader>
+            <TableBody>
+              {assets.map((item, index) => (
+                <TableRow key={index}>
+                  <TableCell><Link href={`/cms/assets/${item.result.uid}`}>{item.result.name}</Link></TableCell>
+                  <TableCell>{moment(item.result.date_created).format('LL')}</TableCell>
+                  <TableCell>{moment(item.result.date_modified).format('LL')}</TableCell>
+                  <TableCell>{item.result.deployment__submission_count}</TableCell>
+                  <TableCell id={`total_${item.result.uid}`}>{item.local}</TableCell>
+                  <TableCell>
+                    <Button variant="destructive" id={`btn_${item.result.uid}`} className="rounded-full" onClick={() => selectUid(item.result.uid)}>Pull</Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        )}
       </Panel>
     </>
   )
